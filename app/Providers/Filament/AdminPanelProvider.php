16ee47017d9d7420da81b0732b412c94ecf15b36
mishrashrutipriya144 <mishrashrutipriya144@gmail.com>
2026-09-11 03:15:54 +0530

    Add logo & change project name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -5,13 +5,9 @@ namespace App\Providers\Filament;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
-use Illuminate\Support\HtmlString;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
-use Filament\Widgets;
-use Livewire\Livewire;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -30,7 +26,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login(\App\Filament\Pages\Auth\Login::class)
             ->passwordReset()
-            ->brandName('E-Taxi')
+            ->brandName('Flynk')
             ->brandLogo(asset('assets/images/logo.png'))
             ->brandLogoHeight('60px')
             ->favicon(asset('assets/images/favicon.png'))
